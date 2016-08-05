b9fafb1dad1e1ce836c8f3dbbfb0614838851411
Justin Lien <lien.justin.m@gmail.com>
2016-08-04 19:59:50 -0700

    feat: add redis caching

--- server/controllers/getJob.js
+++ server/controllers/getJob.js
@@ -1,17 +1,37 @@
 const request = require('request');
 const getIndeed = require('../models/jobs');
 
+const redisClient = require('redis').createClient;
+const redis = redisClient(6379, 'localhost');
+
 exports.post = (req, res) => {
   let jobTitle = req.body.jobTitle,
       city = req.body.city;
 
-  let ip = req.headers['x-forwarded-for'] || 
-      req.connection.remoteAddress || 
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
+  let key = JSON.stringify(req.body);
 
-  let data = getIndeed(jobTitle, city, ip)(res);
-  res.setHeader('Content-Type', 'application/json');
-  return data;
-  res.end();
+  redis.del(key);
+
+  redis.get(key, (err, result) => {
+    res.setHeader('Content-Type', 'application/json');
+    if (result) {
+      console.log('return from redis');
+      res.send(JSON.parse(result));
+      res.end();
+    } else {
+      let ip = req.headers['x-forwarded-for']
+            || req.connection.remoteAddress
+            || req.socket.remoteAddress
+            || req.connection.socket.remoteAddress;
+      console.log('make api call');
+      getIndeed(jobTitle, city, ip)(res).then((res) => {
+        redis.set(key, res.data);
+        res.parse;
+        res.end();
+      }, (err) => {
+        console.log('err: ', err);
+        res.end();
+      });
+    }
+  });
 }
--- server/models/jobs.js
+++ server/models/jobs.js
@@ -1,19 +1,29 @@
 'use strict';
 const request = require('request');
+const Promise = require('promise');
 
 let getIndeed = (query, city, userIP) => {
   query = encodeURI(query);
   city = encodeURI(city);
 
   return (res) => {
-    request('http://api.indeed.com/ads/apisearch?format=json&publisher=' + 
-      process.env.INDEED + '&q=' + 
-      query + '&l=' + 
-      city + '&radius=.5&st=jobsite&jt=fulltime&limit=15&filter=1&latlong=1&co=us&userip=' +
-      userIP + '&v=2', 
-      (error, response, body) => {
-        return res.send(body);
-      });
+    return new Promise((resolve, reject) => {
+      request('http://api.indeed.com/ads/apisearch?format=json&publisher=' + 
+        process.env.INDEED + '&q=' + 
+        query + '&l=' + 
+        city + '&radius=.5&st=jobsite&jt=fulltime&limit=15&filter=1&latlong=1&co=us&userip=' +
+        userIP + '&v=2', 
+        (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              parse: res.send(JSON.parse(body)),
+              data: body
+            });
+          }
+        });
+    })
   }
 };
 
