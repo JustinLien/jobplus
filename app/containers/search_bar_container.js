e65764997ec825f22c2736d950bc2f728ef5df2e
Justin Lien <lien.justin.m@gmail.com>
2016-08-06 17:07:39 -0700

    feat(fix): move axios to action
    - move axis to action to take advantage of redux middleware
    
    - minor code clean up

--- app/containers/search_bar_container.js
+++ app/containers/search_bar_container.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 import { fetchJobs } from '../actions/index';
 
@@ -18,7 +17,6 @@ class SearchBar extends Component {
     this.onJobTitleInputChange = this.onJobTitleInputChange.bind(this);
     this.onLocationInputChange = this.onLocationInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.searchJob = this.searchJob.bind(this);
   }
 
   onJobTitleInputChange(event) {
@@ -31,54 +29,38 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    this.searchJob(this.state.jobTerm, this.state.locationTerm);
+    this.props.fetchJobs(this.state.jobTerm, this.state.locationTerm);
     this.setState({ jobTerm: '' });
     this.setState({ locationTerm: '' });
-  }
-  
-  searchJob(jobTerm, locationTerm) {
-    console.log('logging in searchJob');
-    axios.post('/api/v1/jobs', {
-      jobTitle: jobTerm,
-      city: locationTerm // Hardcoded
-    }).then(function(response) {
-      this.props.fetchJobs(response.data.results);
-      hashHistory.push('results');
-    }.bind(this)).catch(function(error) {
-      console.log(error);
-    });
   }
 
   render() {
     return (
       <form id="searchForm" onSubmit={this.onFormSubmit}>
-        <div className="box">
-          <div className="container-3">
-
-            <div id="searchInputsBoundary">
-              <input 
-                id="search" 
-                className="formSearchInpt"
-                type="search" 
-                results="4" 
-                autoSave="Developer Jobs" 
-                placeholder="Job"
-                value={this.state.jobTerm}
-                onChange={this.onJobTitleInputChange} />
-              
-              <input 
-                id="searchLocation"
-                className="formSearchInpt"
-                type="search"
-                results="4"
-                placeholder="City"
-                autoSave="San Francisco"
-                value={this.state.locationTerm}
-                onChange={this.onLocationInputChange} />
-            </div>
+        <div className="row">
+          <div className="six columns">
+            <input 
+              id="search" 
+              className="u-full-width"
+              type="search" 
+              results="4" 
+              autoSave="Developer Jobs" 
+              placeholder="Job"
+              value={this.state.jobTerm}
+              onChange={this.onJobTitleInputChange} />
           </div>
-
-          <button id="jobSearchSubmitBtn" type="submit">Submit</button>
+          <div className="six columns">
+            <input 
+              id="searchLocation"
+              className="u-full-width"
+              type="search"
+              results="4"
+              placeholder="City"
+              autoSave="San Francisco"
+              value={this.state.locationTerm}
+              onChange={this.onLocationInputChange} />
+          </div>
+          <button id="jobSearchSubmitBtn" className="button-primary" type="submit">Submit</button>
         </div>
       </form>
     );
